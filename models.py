ed5d60941c13769682f43fd5d5a2b3d3ada0b6d0
MikeyBuh <108359661+MikeyBuh@users.noreply.github.com>
2023-02-17 14:40:51 +1200

    Update models.py

--- models.py
+++ models.py
@@ -13,10 +13,11 @@ class User(Base):
     viewed_users = sq.Column(sq.String(length=40))
     full_name = sq.Column(sq.String(length=80), nullable=False)
     user_link = sq.Column(sq.String(length=40), nullable=False)
+
     photo = relationship('Photo', backref='user', passive_deletes=True)
 
     def __str__(self):
-        return f'User {self.id}: {self.user_id}, {self.city}, {self.viewed_users}, ' \
+        return f'User {self.id}: {self.user_id}, {self.city}, {self.viewed_users}' \
                f'{self.full_name}, {self.user_link}'
 
 
@@ -29,11 +30,12 @@ class Photo(Base):
     likes = sq.Column(sq.Integer, nullable=False)
     comments = sq.Column(sq.Integer, nullable=False)
     photo_link = sq.Column(sq.String(length=500), nullable=False)
+
     id_user = sq.Column(sq.Integer, sq.ForeignKey('user.user_id', ondelete='CASCADE'), nullable=False)
 
     def __str__(self):
-        return f'Photo {self.id}: {self.photo_id}, {self.album_id} ' \
-               f'{self.likes}, {self.comments}, {self.photo_link}, {self.user_link}'
+        return f'Photo {self.id}: {self.photo_id}, {self.album_id}' \
+               f'{self.likes}, {self.comments}, {self.photo_link}'
 
 
 def create_tables(engine):
